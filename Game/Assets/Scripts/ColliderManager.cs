00ea0276c5f4fd0b56543ee4d8daecc7e5f4bb59
Jeremy Sapsed <jms737800@gmail.com>
2019-08-03 01:54:06 +0100

    Level cylinders and collider WIP

---
+++ Game/Assets/Scripts/ColliderManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderManager : MonoBehaviour
+{
+    public Vector2[] vertices;
+
+    public Rigidbody cylinderRigidbody;
+    public Transform cylinderTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var collider = gameObject.GetComponent<PolygonCollider2D>();
+        collider.points = vertices;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float circumference = cylinderTransform.localScale.x * Mathf.PI;
+
+        float rotationsPerSecond = cylinderRigidbody.angularVelocity.y / (2 * Mathf.PI);
+
+        float linearVelocity = circumference / rotationsPerSecond;
+
+        transform.position = new Vector3(linearVelocity * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Game/Assets/Scripts/CylinderControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderControl : MonoBehaviour
+{
+    public float rotationalSpeed = 1f;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        rigidbody.AddTorque(new Vector3(0, 3, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
