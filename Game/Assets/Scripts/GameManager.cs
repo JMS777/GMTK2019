996046b95791df839ab7af42471c3c327f226b68
Jeremy Sapsed <jms737800@gmail.com>
2019-08-03 18:37:56 +0100

    New Level and Improved collision maps

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+
+    private bool waitingOnUserInput;
+
+    private GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        waitingOnUserInput = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waitingOnUserInput)
+        {
+            if (Input.GetKeyDown("Fire1"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    } 
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        waitingOnUserInput = true;
+        gameState = GameState.GameOver;
     }
+}
+
+public enum GameState
+{
+    Start, Playing, GameOver, Finished
 }
---
+++ Game/Assets/Scripts/Shredder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            gameManager.GameOver();
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
