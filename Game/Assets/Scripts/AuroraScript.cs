bb019d96f64b0b52e0701ff950c579f81b393cfa
Jeremy Sapsed <jms737800@gmail.com>
2019-08-04 19:32:59 +0100

    Level 2

---
+++ Game/Assets/Scripts/AuroraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuroraScript : MonoBehaviour
+{
+    public float scale = 0.05f;
+    public Transform LevelTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 10 + (LevelTransform.position.y * scale), transform.position.z);
+    }
+}
